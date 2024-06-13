0567a652c8801474378b30ec1d070c4e30438d1b
ekendra-nz <le.pattillo.nz@gmail.com>
2024-06-13 13:06:39 +1200

    hk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <div class="container">
 	<h1 class="h1 text-primary-500">search</h1>
 	<p>with extra bits</p>
+	<p>hare kṛṣṇa hare kṛṣṇa kṛṣṇa kṛṣṇa hare hare <br />hare rāma hare rāma rāma rāma hare hare</p>
 </div>
