7798008fd76f636ecb0935f85727dd6ad39d7eed
ekendra-nz <le.pattillo.nz@gmail.com>
2024-06-18 20:10:10 +1200

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,19 @@
 </script>
 
 <div class="container">
-	<h1 class="h1 text-primary-500">search</h1>
-	<ul>
-		{#each data.books as book}
-			<li>{book.title}</li>
-		{/each}
-	</ul>
+	<div class="search">
+		<label class="label">
+			<span>Input</span>
+			<input class="input" type="text" placeholder="Input" />
+		</label>
+	</div>
+
+	<div class="results">
+		<h1 class="h1 text-primary-500">results</h1>
+		<ul>
+			{#each data.books as book}
+				<li>{book.title}</li>
+			{/each}
+		</ul>
+	</div>
 </div>
