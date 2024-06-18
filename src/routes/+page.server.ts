beb07db6119a0c1de7002039b8ea2c8a9e337b09
ekendra-nz <le.pattillo.nz@gmail.com>
2024-06-18 20:05:14 +1200

    :)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,7 @@
 import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-	console.log('loaded ....');
 	const { data } = await supabase.from('books').select();
-	console.log(data);
 	return {
 		books: data ?? []
 	};
